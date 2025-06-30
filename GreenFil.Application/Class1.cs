69fc33b9c3745ae78a565e608974b87c0bf7f6b1
Anconeyra <anconeyrafsuyo@gmail.com>
2025-06-30 13:15:20 -0500

    feat: saludo con Hola soy Frank en Class1

--- GreenFil.Application/Class1.cs
+++ GreenFil.Application/Class1.cs
@@ -1,5 +1,10 @@
-﻿namespace GreenFil.Application;
-
-public class Class1
+﻿namespace GreenFil.Application
 {
+    public class Class1
+    {
+        public void Saludar()
+        {
+            Console.WriteLine("Hola soy Frank");
+        }
+    }
 }
